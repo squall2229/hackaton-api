b555e12f001a96e5774c5dff06d53fb7ea0049f0
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-14 00:41:49 +0300

    add static

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express")
+const path = require("path")
 const uploadRouter = require("./routes/uploadRouter")
 const {port} = require("./config")
 
 const app = express()
 
 app.use(express.json())
+app.use(express.static('public'));
+app.use('/', express.static(path.join(`${__dirname  }public`)))
 app.use("/upload", uploadRouter)
 
 const start = async () => {
