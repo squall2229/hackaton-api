a5beb57c7cb5787d2e9c31bc1e6ea8a4c221e009
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-14 00:35:25 +0300

    delete auth module

--- controllers/uploaderController.js
+++ controllers/uploaderController.js
@@ -1,16 +1,9 @@
-const User = require("../models/User")
-
 class UploaderController {
   async upload(req, res) {
     try {
       const {file} = req
 
       if (file) {
-        const user = await User.findById(req.user.id)
-
-        user.file = file.path
-
-        await user.save()
         res.json({ message: "success" })
       }
     } catch (error) {
