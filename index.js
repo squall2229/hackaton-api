48849dd902ac37f0e7a063ec3c6ef87a89242939
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-20 23:33:30 +0300

    sort use

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express")
 const path = require("path")
-const mongoose= require("mongoose")
-// const cors = require('cors')
+const cors = require('cors')
 const uploadRouter = require("./routes/uploadRouter")
-const {port, mongoUrl} = require("./config")
+const {port} = require("./config")
 
 const app = express()
 
 app.use(express.json())
-// app.use(cors())
+app.use(cors())
 app.use(express.static('public'));
 app.use('/', express.static(path.join(`${__dirname  }public`)))
 app.use("/upload", uploadRouter)
 
 const start = async () => {
   try {
-    await mongoose.connect(mongoUrl)
-    
-    app.listen(port, () => {
+      app.listen(port, () => {
       // eslint-disable-next-line no-console
       console.log(`server started on port ${port}`)
     })
