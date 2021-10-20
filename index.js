3d8e09cdaa3732b936c200525579fb94613fadca
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-20 23:31:31 +0300

    sort use

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express")
 const path = require("path")
 const mongoose= require("mongoose")
-const cors = require('cors')
+// const cors = require('cors')
 const uploadRouter = require("./routes/uploadRouter")
 const {port, mongoUrl} = require("./config")
 
 const app = express()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use(express.static('public'));
 app.use('/', express.static(path.join(`${__dirname  }public`)))
 app.use("/upload", uploadRouter)
