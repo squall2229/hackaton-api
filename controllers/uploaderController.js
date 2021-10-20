b4d6bb83dfafe94cca4d655baa2e424abf74b3ab
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-21 00:00:48 +0300

    delete uploader2

--- controllers/uploaderController.js
+++ controllers/uploaderController.js
@@ -3,15 +3,18 @@ const UploaderService = require("../services/UploaderService");
 class UploaderController {
   async upload(req, res) {
     try {
-      const {file} = req
+      // const {file} = req
 
-      if (file) {
-        const data = UploaderService.getTextByAudio(file);
+      // if (file) {
+      //   const data = UploaderService.getTextByAudio(file);
 
-        return res.json({ success: 1, data })
-      } 
+      //   return res.json({ success: 1, data })
+      // } 
       
-      return res.status(400).json({ message: "Нет файла", success: 0 })
+      // return res.status(400).json({ message: "Нет файла", success: 0 })
+      const data = UploaderService.getTextByAudio();
+
+      return  res.json({ success: 1, data })
       
     } catch (error) {
       return res.status(400).json({ message: "Ошибка при сохранении файла", success: 0 })
