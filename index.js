0e1f7e74b6233bbb6ba01133db1ee50adf269635
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-23 15:04:14 +0300

    add test endpoint

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require("path")
 const cors = require('cors')
 const uploadRouter = require("./routes/uploadRouter")
+const mockData = require("./__mock__/text.json")
 
 const app = express()
 
@@ -10,6 +11,10 @@ app.use(cors())
 app.use(express.static('public'));
 app.use('/', express.static(path.join(`${__dirname  }public`)))
 app.use("/upload", uploadRouter)
+
+app.post("/test", (req, res) => {
+  res.json({ data: mockData })
+})
 
 const start = async () => {
   try {
