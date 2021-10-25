971cfda87b44e6f4f6f3af31ec8482cad4e54443
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-26 01:28:31 +0300

    fdfgf

--- controllers/downloadController.js
+++ controllers/downloadController.js
@@ -3,15 +3,15 @@ const DownloaderService = require("../services/DownloaderService")
 class DownloadController {
   static async download(req, res) {
     try {
-      const document = await DownloaderService.download({
+      const buffer = await DownloaderService.download({
         annotation: req.body.annotation,
         tags: req.body.tags,
         text: req.body.text,
       })
 
-      res.setHeader('Content-Disposition', 'attachment; filename=MyDocument.docx');
+      // res.setHeader('Content-Disposition', 'attachment; filename=MyDocument.docx');
       res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.wordprocessingml.document")
-      res.send(Buffer.from(document));
+      res.send(buffer);
     } catch (error) {
       return res.status(400).json({ message: error.message })
     }
