047568b7efbdbfeffe2fb7f480f57d9b7f502cbd
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-27 01:56:32 +0300

    add headers

--- index.js
+++ index.js
@@ -1,35 +1,25 @@
 const express = require("express")
-const http = require("http")
 const path = require("path")
-const cors = require('cors')
+const cors = require("cors")
+const longjohn = require("longjohn")
 const uploadRouter = require("./routes/uploadRouter")
 const downloadRouter = require("./routes/dowloadRouter")
 
 const app = express()
+
+longjohn.async_trace_limit = -1
+
 app.use(cors())
 
 app.use(express.json())
 app.use(express.static('public'));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET")
-    return res.status(200).json({})
-  }
-
-  next()
-})
 app.use('/', express.static(path.join(`${__dirname  }public`)))
 app.use("/upload", uploadRouter)
 app.use("/download", downloadRouter)
 
-const server = http.createServer(app);
-
 const start = async () => {
   try {
-    server.listen(3000, () => {
+    app.listen(3000, () => {
       // eslint-disable-next-line no-console
       console.log(`server started on port ${3000}`)
     })
