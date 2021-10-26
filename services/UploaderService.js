55352a6c2ad4b63f26aca905715bf01307618069
Vladislav Chechulin <vladislav.chechulin@smile2impress.com>
2021-10-26 22:48:41 +0300

    add headers

--- services/UploaderService.js
+++ services/UploaderService.js
@@ -14,8 +14,8 @@ ffmpeg.setFfprobePath(ffprobePath);
 
 // const command = ffmpeg();
 
-const httpAgent = new http.Agent({ keepAlive: true, keepAliveMsecs: 60000 });
-const httpsAgent = new https.Agent({ keepAlive: true, keepAliveMsecs: 60000 });
+const httpAgent = new http.Agent({ keepAlive: true, keepAliveMsecs: 60000, maxFreeSockets: 1000 });
+const httpsAgent = new https.Agent({ keepAlive: true, keepAliveMsecs: 60000, maxFreeSockets: 1000 });
 
 class UploaderService {
   static async getTextByAudio(file) {
